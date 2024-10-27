a8b556eb6acd3bcb7225580e4063a1724cc596e5
Fernando Moro <Morofernando99@gmail.com>
2024-10-27 17:48:37 -0300

    added time and collision calculator for hashTable

--- src/HashTable.java
+++ src/HashTable.java
@@ -1,16 +1,20 @@
 import hash_functions.HashFunction;
 import register_code.RegisterCode;
 import register_code.RegisterCodesArray;
+import utils.TimeCalculator;
 
 public class HashTable {
   private int[] table;
   private int length;
   private HashFunction hashFunction;
+  private TimeCalculator timeCalculator;
+  private long colisionCounter = 0;
 
   public HashTable(int length, HashFunction hashFunction){
     this.length = length;
     this.table = new int[length];
     this.hashFunction = hashFunction;
+    this.timeCalculator = new TimeCalculator();
   }
 
   public int getLength(){
@@ -21,14 +25,25 @@ public class HashTable {
     int value = registerCode.getCode();
     int key = hashFunction.hash(value, this.length);
 
+    if (this.table[key] != 0) {
+      this.colisionCounter++;
+    }
+
     this.table[key] = value;
   }
 
   public void insertEach(RegisterCodesArray registerCodesArray){
+    this.colisionCounter = 0;
+    timeCalculator.start();
+
     for (int i = 0; i < registerCodesArray.getLength(); i++) {
       RegisterCode registerCode = registerCodesArray.getRegisterCodes()[i];
       insert(registerCode);
     }
+
+    double duration = timeCalculator.stop();
+    System.out.println("Tempo de inserção: " + duration + "ms");
+    System.out.println("Colisões: " + this.colisionCounter);
   }
 
   public int search(RegisterCode registerCode){
@@ -40,7 +55,7 @@ public class HashTable {
 
   public void print(){
     for (int i = 0; i < this.length; i++) {
-      System.out.println("{ Key: " + i + "}: { Value: " + this.table[i] + " }");
+      System.out.println("{ Key: " + i + " }: { Value: " + this.table[i] + " }");
     }
   }
 }
